1bc3f466b4c193efedcc8cd5d33715f0e3974954
ben <bencrulis@gmail.com>
2021-09-22 19:21:11 +0200

    Added missing files

--- src/display.rs
+++ src/display.rs
@@ -3,12 +3,10 @@ use std::os::raw::c_int;
 use x11rb::connect;
 use x11rb::rust_connection::RustConnection;
 use x11rb::connection::{RequestConnection, Connection};
-use x11rb::protocol::xproto::{get_window_attributes, Screen, VisualClass, ColormapAlloc, create_colormap, CreateWindowAux, EventMask, WindowClass, ClipOrdering, CreateGCAux, create_gc, map_window, ConnectionExt};
+use x11rb::protocol::xproto::{get_window_attributes, Screen, VisualClass, ColormapAlloc, create_colormap, CreateWindowAux, EventMask, WindowClass, ClipOrdering, CreateGCAux, create_gc, map_window, ConnectionExt, ImageFormat, Gcontext, ChangeGCAux, FillRule};
 use x11rb::protocol::xproto::create_window as  cw;
 use x11rb::protocol::shape::{SO, SK};
 use x11rb::protocol::shape::ConnectionExt as _;
-
-use cairo;
 
 use crate::util::find_visual_with_depth;
 
@@ -17,7 +15,8 @@ pub struct Display {
     win_id: u32,
     width: u16,
     height: u16,
-    cairo_context: cairo::Context
+    depth: u8,
+    gc_context: Gcontext
 }
 
 
@@ -48,51 +47,15 @@ impl Display {
         let visual_ob = find_visual_with_depth(&screen.allowed_depths, win_depth, VisualClass::TRUE_COLOR).unwrap();
         let visual_id = visual_ob.visual_id;
 
-        let mut vvisual = x11::xlib::Visual{
-            ext_data: std::ptr::null_mut(),
-            visualid: 0,
-            class: 0,
-            red_mask: 0,
-            green_mask: 0,
-            blue_mask: 0,
-            bits_per_rgb: 0,
-            map_entries: 0
-        };
-
-        let (mut x11_visual, mut dis) = unsafe {
-            let dis = x11::xlib::XOpenDisplay(std::ptr::null());
-
-            let mut visual_info: x11::xlib::XVisualInfo = x11::xlib::XVisualInfo{
-                visual: &mut vvisual,
-                visualid: 0,
-                screen: 0,
-                depth: 0,
-                class: 0,
-                red_mask: 0,
-                green_mask: 0,
-                blue_mask: 0,
-                colormap_size: 0,
-                bits_per_rgb: 0
-            };
-
-            let screen_id = x11::xlib::XDefaultScreen(dis);
-            x11::xlib::XMatchVisualInfo(dis, screen_id, win_depth as i32, x11::xlib::TrueColor, &mut visual_info);
-            (visual_info, dis)
-        };
-
-
-        let visual_id = x11_visual.visualid as u32;
-
-
         create_colormap(&conn,ColormapAlloc::NONE,cm_id,root, visual_id).unwrap().check().unwrap();
 
         let mut win_aux = CreateWindowAux::new();
-        win_aux.background_pixel = Some(0);
+        //win_aux.background_pixel = Some(0);
         win_aux.override_redirect = Some(1);
         win_aux.border_pixel = Some(1);
         win_aux.event_mask = Some(0);
         win_aux.colormap = Some(cm_id);
-        win_aux.background_pixmap = None;
+        //win_aux.background_pixmap = None;
 
         let captured_events: u32 = (EventMask::POINTER_MOTION | EventMask::POINTER_MOTION_HINT | EventMask::BUTTON_PRESS
             | EventMask::KEY_PRESS
@@ -112,117 +75,70 @@ impl Display {
         x11rb::protocol::xfixes::set_window_shape_region(&conn, win_id, SK::INPUT, 0, 0, region_id);
 
         let mut gc_aux = CreateGCAux::new();
-        gc_aux.background = Some(0);
-        gc_aux.foreground = Some(1);
-        //gc_aux.plane_mask = Some(1);
+        //gc_aux.background = Some(0);
+        //gc_aux.foreground = Some(0);
+        //gc_aux.function = Some(x11rb::protocol::xproto::GX::COPY);
+        gc_aux.plane_mask = Some(u32::MAX);
+
 
         create_gc(&conn, gc_id, win_id, &gc_aux).unwrap().check().unwrap();
 
         map_window(&conn, win_id).unwrap().check().unwrap();
-
-        let cairo_surface = unsafe {
-
-            let surf = cairo::ffi::cairo_xlib_surface_create(dis, win_id as u64,
-                                                             x11_visual.visual, width as c_int, height as c_int);
-            cairo::ffi::cairo_xlib_surface_set_size(surf, width as i32, height as i32);
-
-            cairo::Surface::from_raw_full(surf).unwrap()
-        };
-
-
-        let ctx = cairo::Context::new(&cairo_surface).unwrap();
-
-        //let kbg = conn.grab_keyboard(true, win_id, 0u32, GrabMode::ASYNC, GrabMode::ASYNC).unwrap().reply().unwrap();
-        //println!("grabbed keyboard: {:?}", &kbg);
-
-        /*
-        let loop_starting_time = std::time::Instant::now();
-
-        ctx.set_operator(cairo::Operator::Source);
-
-        let mut j = 0;
-        for i in 0.. {
-            let pointer = conn.query_pointer(win_id).unwrap().reply().unwrap();
-            ctx.push_group();
-            ctx.set_source_rgba(0.0, 0.0, 0.0, 0.0);
-            ctx.set_operator(cairo::Operator::Source);
-            //ctx.fill().unwrap();
-            ctx.paint_with_alpha(1.0).unwrap();
-
-            let draw_alpha = ((i as f64)*std::f64::consts::PI/1000.0).sin().abs()*0.5;
-            //println!("draw alpha: {}", draw_alpha);
-
-            //ctx.push_group();
-            ctx.set_source_rgba(1.0, 0.0, 0.0, draw_alpha);
-            //ctx.set_operator(cairo::Operator::Overlay);
-            ctx.rectangle(500.0, 500.0, 300.0, 300.0);
-            //ctx.clip();
-            ctx.fill().unwrap();
-            ctx.paint_with_alpha(1.0).unwrap();
-            ctx.pop_group_to_source().unwrap();
-
-            ctx.paint_with_alpha(1.0).unwrap();
-
-
-            cairo_surface.flush();
-            conn.flush()?;
-
-            std::thread::sleep(std::time::Duration::from_micros(20));
-
-            j = i;
-            if (std::time::Instant::now() - loop_starting_time).as_secs() > 5 {
-                break
-            }
-        }
-        */
 
         Ok(Display{
             conn,
             win_id,
             width,
             height,
-            cairo_context: ctx
+            depth: win_depth,
+            gc_context: gc_id
         })
     }
 
     pub fn screen_pulse_effect(&mut self, number_of_pulses: u8, color: (f64, f64, f64), alpha: f64) {
 
-        const TICKS_PER_PULSE: u32 = 1000;
+        const TICKS_PER_PULSE: u32 = 200;
         let total_ticks: u32 = TICKS_PER_PULSE*number_of_pulses as u32;
 
         let loop_starting_time = std::time::Instant::now();
 
-        let ctx = &self.cairo_context;
-        let cairo_surface = ctx.target();
-
-        ctx.set_operator(cairo::Operator::Source);
-
         for i in 0..total_ticks {
-            let pointer = self.conn.query_pointer(self.win_id).unwrap().reply().unwrap();
-            ctx.push_group();
-            ctx.set_source_rgba(0.0, 0.0, 0.0, 0.0);
-            ctx.set_operator(cairo::Operator::Source);
-            //ctx.fill().unwrap();
-            ctx.paint_with_alpha(1.0).unwrap();
+            //let pointer = self.conn.query_pointer(self.win_id).unwrap().reply().unwrap();
 
-            let draw_alpha = ((i as f64)*std::f64::consts::PI/TICKS_PER_PULSE as f64).sin().abs()*alpha;
+            let draw_alpha = (((i as f64)*std::f64::consts::PI/TICKS_PER_PULSE as f64).sin().abs()*alpha*255.0) as u8;
             //println!("draw alpha: {}", draw_alpha);
 
-            //ctx.push_group();
-            ctx.set_source_rgba(color.0, color.1, color.2, draw_alpha);
-            //ctx.set_operator(cairo::Operator::Overlay);
-            ctx.rectangle(500.0, 500.0, 300.0, 300.0);
-            //ctx.clip();
-            ctx.fill().unwrap();
-            ctx.paint_with_alpha(1.0).unwrap();
-            ctx.pop_group_to_source().unwrap();
+            /*
+            self.conn.put_image(ImageFormat::Z_PIXMAP, self.win_id, self.gc_context,
+                                self.width, self.height, 0, 0, 0, self.depth, &rect);
 
-            ctx.paint_with_alpha(1.0).unwrap();
+             */
 
-            cairo_surface.flush();
+            let mut ch_gc = x11rb::protocol::xproto::ChangeGCAux::new();
+
+            let red = draw_alpha as u32;
+            let green = 0;
+            let blue = 0;
+            let color: u32 = (blue << 0) | (green << 8) | (red << 16) | ((draw_alpha as u32) << 24);
+
+            ch_gc.foreground = Some(color);
+
+            //println!("color: {:032b}", color);
+
+            self.conn.change_gc(self.gc_context, &ch_gc).unwrap().check().unwrap();
+
+            //self.conn.clear_area(false, self.win_id, 0, 0, self.width, self.height).unwrap().check().unwrap();
+            self.conn.poly_fill_rectangle(self.win_id, self.gc_context, &[x11rb::protocol::xproto::Rectangle{
+                x: 0,
+                y: 0,
+                width: self.width,
+                height: self.height
+            }]);
+
             self.conn.flush().unwrap();
 
-            std::thread::sleep(std::time::Duration::from_micros(20));
+            std::thread::sleep(std::time::Duration::from_micros(5000));
+            //std::thread::sleep(std::time::Duration::from_secs(1));
 
         }
     }
--- src/util.rs
+++ src/util.rs
@@ -16,21 +16,6 @@ pub fn find_visual_with_depth(visuals: &[Depth], depth: u8, visual_class: Visual
 }
 
 
-pub fn visual_type_to_x11visual(visualtype: Visualtype, ext_data: *mut x11::xlib::XExtData) -> x11::xlib::Visual {
-    let visual_class: u8 = visualtype.class.into();
-    x11::xlib::Visual{
-        ext_data,
-        visualid: visualtype.visual_id as u64,
-        class: visual_class as c_int,
-        red_mask: visualtype.red_mask as u64,
-        green_mask: visualtype.green_mask as u64,
-        blue_mask: visualtype.blue_mask as u64,
-        bits_per_rgb: visualtype.bits_per_rgb_value as c_int,
-        map_entries: visualtype.colormap_entries as c_int
-    }
-}
-
-
 pub fn slice_to_sequence_buffer(slice: &[u8]) -> [u8; 32] {
     let mut buffer = [0; 32];
     for (i, v) in slice.iter().enumerate() {
