eeb1d4ddd127fa2f3346c3d5077135a516a14c94
ben <bencrulis@gmail.com>
2021-09-25 19:42:03 +0200

    Implemented `list` command to list PIDs of current reminders.

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,17 @@ mod util;
 use std::error::Error;
 use std::time::Instant;
 
+use std::fs::File;
+
 use clap::{App, Arg};
 use time;
 use chrono;
 use fork;
 
 use crate::util::find_visual_with_depth;
+use sysinfo::{SystemExt, ProcessExt};
+use std::fmt::Debug;
+use std::io::{Write, Read};
 
 
 fn set_remainder(nb_seconds: u64) -> Result<(), Box<dyn Error>> {
@@ -18,6 +23,33 @@ fn set_remainder(nb_seconds: u64) -> Result<(), Box<dyn Error>> {
     let mut display_obj = display::Display::create_and_connect()?;
     display_obj.default_screen_pulse_effect();
     Ok(())
+}
+
+
+fn list_alike() {
+    let own_pid = sysinfo::get_current_pid().unwrap();
+    let mut system = sysinfo::System::new_all();
+    system.refresh_system();
+
+    //println!("own process: {}", own_pid);
+
+    let exe_name = "visual_alarm";
+
+    for (pid, _) in system.processes().iter().filter(|(&pid, process)| process.name() == exe_name && pid != own_pid) {
+        //print!("{}: {}", pid, process.name());
+        //println!("{:?}", process.cmd());
+        let path = format!("/proc/{}/fd/3", pid);
+        let mut file = std::fs::OpenOptions::new().read(true).open(&path).unwrap();
+        //let link = std::fs::read_link(&path).unwrap();
+        //println!("link: {}", link.to_str().unwrap());
+        //std::fs::remove_file(filepath).unwrap();
+        let mut buffer = String::new();
+        file.read_to_string(&mut buffer);
+        //println!("content of file: {}", buffer);
+        println!("{}: {}", &pid, buffer);
+
+    }
+
 }
 
 
@@ -40,11 +72,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             )
         .get_matches();
 
+    let filepath = "/tmp/test_visual_alarm";
+
+
     let nb_minutes_str = matches.value_of("NB_MINUTES").unwrap();
 
     let no_child = matches.is_present("no_child");
 
     if let Some(nb_minutes) = nb_minutes_str.parse::<u16>().ok() {
+        let mut file = std::fs::OpenOptions::new().create_new(true).write(true).truncate(true).open(filepath).unwrap();
+        std::fs::remove_file(filepath).unwrap();
+
         let nb_seconds = nb_minutes as u64 * 60;
 
         let mut call_time = chrono::Local::now();
@@ -52,6 +90,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let remind_time = (call_time + chrono::FixedOffset::east(nb_seconds as i32));
 
         println!("Reminder set for now+{} minutes: {}", nb_minutes, remind_time);
+
+        file.write_all(format!("set for {}", remind_time).as_bytes());
+        file.flush();
+        //println!("written to file");
 
         if no_child {
             set_remainder(nb_seconds)?;
@@ -67,6 +109,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             }
         }
+    }
+    else if nb_minutes_str == "list" {
+        list_alike();
     }
     else {
         println!("incorrect number of minutes: \"{}\"\nexiting...", nb_minutes_str);
